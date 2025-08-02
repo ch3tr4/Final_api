73bb5a6e0ae24344898014c24d28f3842fe20f7f
ch3tr4 <ch3tr4@gmail.com>
2025-08-02 11:52:28 +0700

    Dynamic

--- hvac_master/shop/admin.py
+++ hvac_master/shop/admin.py
@@ -1,16 +1,48 @@
 from django.contrib import admin
 from .models import *
 
+class AdminMenu(admin.ModelAdmin):
+    list_display = [
+        'menuName', 
+    ] 
+    list_filter = [
+        'menuName', 
+    ] 
+    search_fields = [
+        'menuName', 
+    ] 
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        self.opts.verbose_name_plural="1. Menu " 
+
+class AdminSubMenu(admin.ModelAdmin):
+    list_display = [
+        'menuName', 
+    ] 
+    list_filter = [
+        'menuName', 
+    ] 
+    search_fields = [
+        'menuName', 
+    ] 
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        self.opts.verbose_name_plural="1. Sub Menu  " 
+
+admin.site.register(TopHeader)
+admin.site.register(Menu,AdminMenu) 
+admin.site.register(SubMenu,AdminSubMenu) 
+admin.site.register(Sub2Menu)
+admin.site.register(HomeBanner)
+admin.site.register(Service)
+admin.site.register(Feature)
 admin.site.register(Car)
 admin.site.register(CarImage)
-admin.site.register(TopHeader)
+admin.site.register(ChooseUs)
+admin.site.register(LastestBlog)
 admin.site.register(FooterSection)
-admin.site.register(Feature)
-admin.site.register(Menu)
-admin.site.register(MenuDetail)
-admin.site.register(Menu2)
-admin.site.register(MenuDetail2)
-admin.site.register(TitleBanner)
-admin.site.register(TitleBanner2)
+
+# admin.site.register(TitleBanner)
+# admin.site.register(TitleBanner2)
 
 
